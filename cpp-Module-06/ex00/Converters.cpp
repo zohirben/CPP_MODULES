0bee17151fbbbb265ea59bf5c3ed7814f9fdb937
Zouhair Benaissa <zbenaiss@c2r7p6.1337.ma>
2024-05-06 23:19:51 +0100

    uppddate

--- cpp-Module-06/ex00/Converters.cpp
+++ cpp-Module-06/ex00/Converters.cpp
@@ -1,32 +1,54 @@
 #include "ScalarConverter.hpp"
 
-
-void ConvertChar(std::string input)
-{
-    char c = input[0];
-    std::cout << c << std::endl;
-    
-}
-
 void ConvertInt(std::string input)
 {
+    char c;
+    int i;
+    float f;
+    double d;
     int numInt = std::stoi(input);
     std::cout << numInt << std::endl;
+    
+    c = static_cast<char>(numInt);
+    i = static_cast<int>(numInt);
+    f = static_cast<float>(numInt);
+    d = static_cast<double>(numInt);
+
     
 }
 
 void ConvertFloat(std::string input)
 {
+    char c;
+    int i;
+    float f;
+    double d;
     float numFloat = std::stof(input);
     std::cout << numFloat << std::endl;
+    
+    c = static_cast<char>(numFloat);
+    i = static_cast<int>(numFloat);
+    f = static_cast<float>(numFloat);
+    d = static_cast<double>(numFloat);
+
     
 }
 
 void ConvertDouble(std::string input)
 {
+    char c;
+    int i;
+    float f;
+    double d;
     double numDouble = std::stod(input);
     std::cout << numDouble << std::endl;
 
+    c = static_cast<char>(numDouble);
+    i = static_cast<int>(numDouble);
+    f = static_cast<float>(numDouble);
+    d = static_cast<double>(numDouble);
+
+    
 }
 
 void printAll(char c, int x, float y, double z)
