7e5edf0153ebc0b81c1164c84e614616afad8ee7
Zouhair Benaissa <zbenaiss@c2r11p2.1337.ma>
2024-05-18 19:56:58 +0100

    was joking hh, dynamic casting done

--- cpp-Module-06/ex02/Base.cpp
+++ cpp-Module-06/ex02/Base.cpp
@@ -3,3 +3,63 @@
 Base::~Base()
 {
 }
+
+Base *generate(void)
+{
+    std::srand(std::time(0));
+    int random = 2;
+    random = std::rand() % 3;
+    switch (random)
+    {
+    case 0:
+        return new A();
+    case 1:
+        return new B();
+    case 2:
+        return new C();
+    }
+    return NULL;
+}
+
+void identify(Base *p)
+{
+    if (dynamic_cast<A *>(p) != NULL)
+        std::cout << "Object Pointed By p is A" << std::endl;
+    else if (dynamic_cast<B *>(p) != NULL)
+        std::cout << "Object Pointed By p is B" << std::endl;
+    else if (dynamic_cast<C *>(p) != NULL)
+        std::cout << "Object Pointed By p is C" << std::endl;
+    else
+        std::cout << "Unknown Type" << std::endl;
+}
+
+void identify(Base &p)
+{
+    try
+    {
+        A &a = dynamic_cast<A&>(p);
+        std::cout << "Object Refferenced By p is A" << std::endl;
+        (void)a;
+    }
+    catch (std::bad_cast)
+    {
+    }
+    try
+    {
+        B &b = dynamic_cast<B&>(p);
+        std::cout << "Object Refferenced By p is B" << std::endl;
+        (void)b;
+    }
+    catch (std::bad_cast)
+    {
+    }
+    try
+    {
+        C &c = dynamic_cast<C&>(p);
+        std::cout << "Object Refferenced By p is C" << std::endl;
+        (void)c;
+    }
+    catch (std::bad_cast)
+    {
+    }
+}
--- cpp-Module-06/ex02/Base.hpp
+++ cpp-Module-06/ex02/Base.hpp
@@ -2,9 +2,18 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 class Base {
     public:
         virtual ~Base();
-        
 };
+
+
+class A : public Base{};
+class B : public Base{};
+class C : public Base{};
+
+Base * generate(void);
+void identify(Base* p);
+void identify(Base& p);
